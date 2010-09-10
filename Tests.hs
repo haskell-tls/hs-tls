c1b21f6a24679175e8d555f2b30013439ae863b1
Vincent Hanquez <vincent@snarc.org>
2010-09-10 22:32:55 +0100

    move tests to quickcheck2

--- Tests.hs
+++ Tests.hs
@@ -1,11 +1,12 @@
 import Text.Printf
 import Data.Word
 import Test.QuickCheck
-import Test.QuickCheck.Batch
+import Test.QuickCheck.Test
 
 import Network.TLS.Struct
 import Network.TLS.Packet
 import Control.Monad
+import System.IO
 
 liftM6 f m1 m2 m3 m4 m5 m6 = do { x1 <- m1; x2 <- m2; x3 <- m3; x4 <- m4; x5 <- m5; x6 <- m6; return (f x1 x2 x3 x4 x5 x6) }
 
@@ -73,13 +74,15 @@ prop_handshake_marshalling_id x = (decodeHs $ encodeHandshake x) == Right x
 		decodeHs b = either (Left . id) (\(ty, bdata) -> decodeHandshake TLS10 ty bdata) $ decodeHandshakeHeader b
 
 {- main -}
-options = TestOptions
-	{ no_of_tests         = 2000
-	, length_of_tests     = 1
-	, debug_tests         = False }
+args = Args
+	{ replay     = Nothing
+	, maxSuccess = 500
+	, maxDiscard = 2000
+	, maxSize    = 500
+	}
+
+run_test n t = putStr ("  " ++ n ++ " ... ") >> hFlush stdout >> quickCheckWith args t
 
 main = do
-	runTests "marshalling=id" options
-		[ run prop_header_marshalling_id
-		, run prop_handshake_marshalling_id
-		]
+	run_test "marshalling header = id" prop_header_marshalling_id
+	run_test "marshalling handshake = id" prop_handshake_marshalling_id
