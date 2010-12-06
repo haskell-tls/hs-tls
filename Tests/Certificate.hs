bec3255ff3280c0ee2f0c6b26da4003e3069536b
Vincent Hanquez <vincent@snarc.org>
2010-12-06 08:18:16 +0000

    add a (non-valid) signature, otherwise test fail.

--- Tests/Certificate.hs
+++ Tests/Certificate.hs
@@ -39,6 +39,7 @@ arbitraryX509 pubKey@(X509.PubKey alg _) = do
 	subjectdn <- arbitraryDN
 	time1     <- arbitraryTime
 	time2     <- arbitraryTime
+	sig       <- resize 40 $ listOf1 arbitrary
 	return $ X509.Certificate
 		{ X509.certVersion      = version
 		, X509.certSerial       = serial
@@ -48,6 +49,6 @@ arbitraryX509 pubKey@(X509.PubKey alg _) = do
 		, X509.certValidity     = (time1, time2)
 		, X509.certPubKey       = pubKey
 		, X509.certExtensions   = Nothing
-		, X509.certSignature    = Nothing
+		, X509.certSignature    = Just (alg, sig)
 		, X509.certOthers       = []
 		}
