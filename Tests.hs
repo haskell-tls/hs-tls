18cf6a5392345e72ff907320509efe03307e75e3
Vincent Hanquez <vincent@snarc.org>
2010-12-07 09:15:34 +0000

    test ciphers in a basic fashion for now.

--- Tests.hs
+++ Tests.hs
@@ -2,7 +2,9 @@
 
 import qualified Tests.Marshal as Marshal
 import qualified Tests.Connection as Connection
+import qualified Tests.Ciphers as Ciphers
 
 main = do
 	Marshal.runTests
+	Ciphers.runTests
 	Connection.runTests
