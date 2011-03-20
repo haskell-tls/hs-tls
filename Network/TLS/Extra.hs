511a7817215e38e6d2b420ea287b9737a57947bd
Vincent Hanquez <vincent@snarc.org>
2011-03-20 07:07:57 +0000

    export certificate checking in extra

--- Network/TLS/Extra.hs
+++ Network/TLS/Extra.hs
@@ -1,5 +1,7 @@
 module Network.TLS.Extra 
 	( module Network.TLS.Extra.Cipher
+	, module Network.TLS.Extra.Certificate
 	) where
 
 import Network.TLS.Extra.Cipher
+import Network.TLS.Extra.Certificate
