e47501736b0d9b687a310dbba09696ee10c5dd51
Vincent Hanquez <vincent@snarc.org>
2012-07-12 09:03:24 +0100

    add usual header

--- Network/TLS/Extra/Compression.hs
+++ Network/TLS/Extra/Compression.hs
@@ -1,3 +1,10 @@
+-- |
+-- Module      : Network.TLS.Extra.Compression
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : unknown
+--
 module Network.TLS.Extra.Compression
 	(
 	) where
