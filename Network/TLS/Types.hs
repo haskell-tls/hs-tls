ce3f7245642db1a9cc52e5cde1b56a17a1abcbd5
Vincent Hanquez <vincent@snarc.org>
2012-07-12 09:03:48 +0100

    add usual header.

--- Network/TLS/Types.hs
+++ Network/TLS/Types.hs
@@ -1,3 +1,10 @@
+-- |
+-- Module      : Network.TLS.Types
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : unknown
+--
 module Network.TLS.Types
     ( Version(..)
     , SessionID
