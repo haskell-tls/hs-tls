975fc32889e4f52eea8f3c5363e28867b69cc71b
Vincent Hanquez <vincent@snarc.org>
2012-08-18 23:05:37 +0100

    split signature apart from handshake

--- Network/TLS/Handshake.hs
+++ Network/TLS/Handshake.hs
@@ -35,16 +35,12 @@ import Data.ByteString.Char8 ()
 
 import Data.Certificate.X509(X509, certSubjectDN, x509Cert, certPubKey, PubKey(PubKeyRSA))
 
-import qualified Crypto.Hash.SHA224 as SHA224
-import qualified Crypto.Hash.SHA256 as SHA256
-import qualified Crypto.Hash.SHA384 as SHA384
-import qualified Crypto.Hash.SHA512 as SHA512
-
 import Control.Applicative ((<$>))
 import Control.Monad.State
 import Control.Exception (Exception(), fromException, catch, SomeException)
 import Prelude hiding (catch)
 
+import Network.TLS.Handshake.Signature
 import Network.TLS.Handshake.Common
 import Network.TLS.Handshake.Certificate
 
@@ -258,20 +254,6 @@ handshakeClient cparams ctx = do
                                         else encodeWord16 $ fromIntegral $ B.length e
                                 return $ extra `B.append` e
                         sendPacket ctx $ Handshake [ClientKeyXchg encryptedPreMaster]
-
-getHashAndASN1 :: MonadIO m => (HashAlgorithm, SignatureAlgorithm) -> m (B.ByteString -> B.ByteString, B.ByteString)
-getHashAndASN1 hashSig = do
-  case hashSig of
-    (HashSHA224, SignatureRSA) ->
-      return (SHA224.hash, "\x30\x2d\x30\x0d\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x04\x05\x00\x04\x1c")
-    (HashSHA256, SignatureRSA) ->
-      return (SHA256.hash, "\x30\x31\x30\x0d\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x01\x05\x00\x04\x20")
-    (HashSHA384, SignatureRSA) ->
-      return (SHA384.hash, "\x30\x41\x30\x0d\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x02\x05\x00\x04\x30")
-    (HashSHA512, SignatureRSA) ->
-      return (SHA512.hash, "\x30\x51\x30\x0d\x06\x09\x60\x86\x48\x01\x65\x03\x04\x02\x03\x05\x00\x04\x40")
-    _ ->
-      throwCore $ Error_Misc "unsupported hash/sig algorithm"
 
 
 handshakeServerWith :: MonadIO m => ServerParams -> Context -> Handshake -> m ()
