acc4ef5ca5bf68bb9cc2bdc96e3810f43008ea7a
Vincent Hanquez <vincent@snarc.org>
2012-10-17 07:30:41 +0100

    add MaxFragmentLength extension

--- core/Network/TLS/Extension.hs
+++ core/Network/TLS/Extension.hs
@@ -12,10 +12,13 @@ module Network.TLS.Extension
     , supportedExtensions
     -- all extensions ID supported
     , extensionID_ServerName
+    , extensionID_MaxFragmentLength
     , extensionID_SecureRenegotiation
     , extensionID_NextProtocolNegotiation
     -- all implemented extensions
     , ServerName(..)
+    , MaxFragmentLength(..)
+    , MaxFragmentEnum(..)
     , SecureRenegotiation(..)
     , NextProtocolNegotiation(..)
     ) where
@@ -31,14 +34,18 @@ import qualified Data.ByteString as B
 import Network.TLS.Struct (ExtensionID)
 import Network.TLS.Wire
 
-extensionID_SecureRenegotiation, extensionID_NextProtocolNegotiation :: ExtensionID
+extensionID_ServerName, extensionID_MaxFragmentLength
+                      , extensionID_SecureRenegotiation
+                      , extensionID_NextProtocolNegotiation :: ExtensionID
 extensionID_ServerName              = 0x0
+extensionID_MaxFragmentLength       = 0x1
 extensionID_SecureRenegotiation     = 0xff01
 extensionID_NextProtocolNegotiation = 0x3374
 
 -- | all supported extensions by the implementation
 supportedExtensions :: [ExtensionID]
 supportedExtensions = [ extensionID_ServerName
+                      , extensionID_MaxFragmentLength
                       , extensionID_SecureRenegotiation
                       , extensionID_NextProtocolNegotiation
                       ]
@@ -48,7 +55,6 @@ class Extension a where
     extensionID     :: a -> ExtensionID
     extensionDecode :: Bool -> ByteString -> Maybe a
     extensionEncode :: a -> ByteString
-
 
 -- | Server Name extension including the name type and the associated name.
 -- the associated name decoding is dependant of its name type.
@@ -65,6 +71,26 @@ instance Extension ServerName where
                   ty    <- getWord8
                   sname <- getOpaque16
                   return (1+2+B.length sname, (ty, sname))
+
+-- | Max fragment extension with length from 512 bytes to 4096 bytes
+data MaxFragmentLength = MaxFragmentLength MaxFragentEnum
+    deriving (Show,Eq)
+data MaxFragmentEnum = MaxFragment512 | MaxFragment1024 | MaxFragment2048 | MaxFragment4096
+    deriving (Show,Eq)
+
+instance Extension MaxFragmentLength where
+    extensionID _ = extensionID_MaxFragmentLength
+    extensionEncode (MaxFragmentLength e) = putWord8 $ marshallSize e
+        where marshallSize MaxFragment512  = 1
+              marshallSize MaxFragment1024 = 2
+              marshallSize MaxFragment2048 = 3
+              marshallSize MaxFragment4096 = 4
+    extensionDecode _ = runGetMaybe (unmarshallSize <$> getWord8)
+        where unmarshallSize 1 = MaxFragment512
+              unmarshallSize 2 = MaxFragment1024
+              unmarshallSize 3 = MaxFragment2048
+              unmarshallSize 4 = MaxFragment4096
+              unmarshallSize _ = error ("unknown max fragment size " ++ show n)
 
 -- | Secure Renegotiation
 data SecureRenegotiation = SecureRenegotiation ByteString (Maybe ByteString)
