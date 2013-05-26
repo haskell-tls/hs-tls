59d61067b04081b0dc94f130ae0d02776d944aae
Vincent Hanquez <vincent@snarc.org>
2013-05-26 07:19:59 +0100

    add X509 file.

---
+++ core/Network/TLS/X509.hs
@@ -0,0 +1,21 @@
+-- |
+-- Module      : Network.TLS.X509
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- X509 helpers
+--
+module Network.TLS.X509
+    ( CertificateChain(..)
+    , Certificate(..)
+    , SignedCertificate
+    , getCertificate
+    , isNullCertificateChain
+    ) where
+
+import Data.X509
+
+isNullCertificateChain :: CertificateChain -> Bool
+isNullCertificateChain (CertificateChain l) = null l
