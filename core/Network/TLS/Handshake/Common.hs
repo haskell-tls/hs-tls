290d98d95b36c041d08ff22295eab0adc93e8afd
Vincent Hanquez <vincent@snarc.org>
2013-07-10 07:37:52 +0100

    re-indent

--- core/Network/TLS/Handshake/Common.hs
+++ core/Network/TLS/Handshake/Common.hs
@@ -30,7 +30,7 @@ import Control.Monad.State
 import Control.Exception (throwIO, Exception())
 
 data HandshakeFailed = HandshakeFailed TLSError
-        deriving (Show,Eq,Typeable)
+    deriving (Show,Eq,Typeable)
 
 instance Exception HandshakeFailed
 
@@ -46,34 +46,34 @@ unexpected msg expected = throwCore $ Error_Packet_unexpected msg (maybe "" (" e
 
 newSession :: MonadIO m => Context -> m Session
 newSession ctx
-        | pUseSession $ ctxParams ctx = getStateRNG ctx 32 >>= return . Session . Just
-        | otherwise                   = return $ Session Nothing
+    | pUseSession $ ctxParams ctx = getStateRNG ctx 32 >>= return . Session . Just
+    | otherwise                   = return $ Session Nothing
 
 -- | when a new handshake is done, wrap up & clean up.
 handshakeTerminate :: MonadIO m => Context -> m ()
 handshakeTerminate ctx = do
-        session <- usingState_ ctx getSession
-        -- only callback the session established if we have a session
-        case session of
-                Session (Just sessionId) -> do
-                        sessionData <- usingState_ ctx getSessionData
-                        withSessionManager (ctxParams ctx) (\s -> liftIO $ sessionEstablish s sessionId (fromJust sessionData))
-                _ -> return ()
-        -- forget all handshake data now and reset bytes counters.
-        usingState_ ctx endHandshake
-        updateMeasure ctx resetBytesCounters
-        -- mark the secure connection up and running.
-        setEstablished ctx True
-        return ()
+    session <- usingState_ ctx getSession
+    -- only callback the session established if we have a session
+    case session of
+        Session (Just sessionId) -> do
+            sessionData <- usingState_ ctx getSessionData
+            withSessionManager (ctxParams ctx) (\s -> liftIO $ sessionEstablish s sessionId (fromJust sessionData))
+        _ -> return ()
+    -- forget all handshake data now and reset bytes counters.
+    usingState_ ctx endHandshake
+    updateMeasure ctx resetBytesCounters
+    -- mark the secure connection up and running.
+    setEstablished ctx True
+    return ()
 
 sendChangeCipherAndFinish :: MonadIO m => Context -> Bool -> m ()
 sendChangeCipherAndFinish ctx isClient = do
-        sendPacket ctx ChangeCipherSpec
+    sendPacket ctx ChangeCipherSpec
 
-        when isClient $ do
-          let cparams = getClientParams $ ctxParams ctx
-          suggest <- usingState_ ctx $ getServerNextProtocolSuggest
-          case (onNPNServerSuggest cparams, suggest) of
+    when isClient $ do
+        let cparams = getClientParams $ ctxParams ctx
+        suggest <- usingState_ ctx $ getServerNextProtocolSuggest
+        case (onNPNServerSuggest cparams, suggest) of
             -- client offered, server picked up. send NPN handshake.
             (Just io, Just protos) -> do proto <- liftIO $ io protos
                                          sendPacket ctx (Handshake [HsNextProtocolNegotiation proto])
@@ -82,43 +82,42 @@ sendChangeCipherAndFinish ctx isClient = do
             (Just _, Nothing) -> return ()
             -- client didn't offer. do nothing.
             (Nothing, _) -> return ()
-        liftIO $ contextFlush ctx
+    liftIO $ contextFlush ctx
 
-        cf <- usingState_ ctx $ getHandshakeDigest isClient
-        sendPacket ctx (Handshake [Finished cf])
-        liftIO $ contextFlush ctx
+    cf <- usingState_ ctx $ getHandshakeDigest isClient
+    sendPacket ctx (Handshake [Finished cf])
+    liftIO $ contextFlush ctx
 
 recvChangeCipherAndFinish :: MonadIO m => Context -> m ()
 recvChangeCipherAndFinish ctx = runRecvState ctx (RecvStateNext expectChangeCipher)
-        where
-                expectChangeCipher ChangeCipherSpec = return $ RecvStateHandshake expectFinish
-                expectChangeCipher p                = unexpected (show p) (Just "change cipher")
-                expectFinish (Finished _) = return RecvStateDone
-                expectFinish p            = unexpected (show p) (Just "Handshake Finished")
+  where expectChangeCipher ChangeCipherSpec = return $ RecvStateHandshake expectFinish
+        expectChangeCipher p                = unexpected (show p) (Just "change cipher")
+        expectFinish (Finished _) = return RecvStateDone
+        expectFinish p            = unexpected (show p) (Just "Handshake Finished")
 
 data RecvState m =
-          RecvStateNext (Packet -> m (RecvState m))
-        | RecvStateHandshake (Handshake -> m (RecvState m))
-        | RecvStateDone
+      RecvStateNext (Packet -> m (RecvState m))
+    | RecvStateHandshake (Handshake -> m (RecvState m))
+    | RecvStateDone
 
 recvPacketHandshake :: MonadIO m => Context -> m [Handshake]
 recvPacketHandshake ctx = do
-        pkts <- recvPacket ctx
-        case pkts of
-                Right (Handshake l) -> return l
-                Right x             -> fail ("unexpected type received. expecting handshake and got: " ++ show x)
-                Left err            -> throwCore err
+    pkts <- recvPacket ctx
+    case pkts of
+        Right (Handshake l) -> return l
+        Right x             -> fail ("unexpected type received. expecting handshake and got: " ++ show x)
+        Left err            -> throwCore err
 
 runRecvState :: MonadIO m => Context -> RecvState m -> m ()
 runRecvState _   (RecvStateDone)   = return ()
 runRecvState ctx (RecvStateNext f) = recvPacket ctx >>= either throwCore f >>= runRecvState ctx
 runRecvState ctx iniState          = recvPacketHandshake ctx >>= loop iniState >>= runRecvState ctx
-        where
-                loop :: MonadIO m => RecvState m -> [Handshake] -> m (RecvState m)
-                loop recvState []                  = return recvState
-                loop (RecvStateHandshake f) (x:xs) = do
-                        nstate <- f x
-                        usingState_ ctx $ processHandshake x
-                        loop nstate xs
-                loop _                         _   = unexpected "spurious handshake" Nothing
+  where
+        loop :: MonadIO m => RecvState m -> [Handshake] -> m (RecvState m)
+        loop recvState []                  = return recvState
+        loop (RecvStateHandshake f) (x:xs) = do
+            nstate <- f x
+            usingState_ ctx $ processHandshake x
+            loop nstate xs
+        loop _                         _   = unexpected "spurious handshake" Nothing
 
