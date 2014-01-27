8b03b9ca86d0b6f60c491392fd73a59bf6036e11
Vincent Hanquez <vincent@snarc.org>
2014-01-27 04:03:26 +0000

    add Network.TLS.Extra for best compatibility with tls-extra removal

---
+++ core/Network/TLS/Extra.hs
@@ -0,0 +1,13 @@
+-- |
+-- Module      : Network.TLS.Extra
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>
+-- Stability   : experimental
+-- Portability : unknown
+--
+-- default values and ciphers
+module Network.TLS.Extra
+    ( module Network.TLS.Extra.Cipher
+    ) where
+
+import Network.TLS.Extra.Cipher
