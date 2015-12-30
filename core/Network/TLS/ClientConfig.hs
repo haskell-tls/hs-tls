e02c6c6d4f01d00dec1844257ba74b4938748688
Toshio Ito <debug.ito@gmail.com>
2015-12-30 10:41:05 +0900

    Add Network.TLS.ClientConfig. It's empty yet.

---
+++ core/Network/TLS/ClientConfig.hs
@@ -0,0 +1,10 @@
+-- |
+-- Module      : Network.TLS.Parameters
+-- License     : BSD-style
+-- Maintainer  : Vincent Hanquez <vincent@snarc.org>, Toshio Ito <debug.ito@gmail.com>
+-- Stability   : experimental
+-- Portability : unknown
+--
+module Network.TLS.ClientConfig (
+  
+) where
