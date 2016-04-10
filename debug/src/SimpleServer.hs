0493032f607ad80094021e8bf00a100519930ee2
Vincent Hanquez <vincent@snarc.org>
2016-04-10 08:11:05 +0100

    fix compilation of simple server on old ghc

--- debug/src/SimpleServer.hs
+++ debug/src/SimpleServer.hs
@@ -130,7 +130,7 @@ getDefaultParams flags host store sStorage cred session =
                     l  -> cipherID c `elem` l
 
             getDebugSeed :: Maybe Seed -> Flag -> Maybe Seed
-            getDebugSeed _   (DebugSeed seed) = seedFromInteger <$> readNumber seed
+            getDebugSeed _   (DebugSeed seed) = seedFromInteger `fmap` readNumber seed
             getDebugSeed acc _                = acc
 
             tlsConnectVer
