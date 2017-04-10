37031fc1e47cda6f10c7ab3a45997f0d2d7343ba
Kazu Yamamoto <kazu@iij.ad.jp>
2017-04-10 15:54:32 +0900

    Exporting Group.

--- core/Network/TLS.hs
+++ core/Network/TLS.hs
@@ -125,6 +125,9 @@ module Network.TLS
     , ValidationCache(..)
     , ValidationCacheResult(..)
     , exceptionValidationCache
+
+    -- * Key exchange group
+    , Group(..)
     ) where
 
 import Network.TLS.Backend (Backend(..), HasBackend(..))
@@ -136,6 +139,7 @@ import Network.TLS.Struct ( TLSError(..), TLSException(..)
                           , Bytes
                           , Handshake)
 import Network.TLS.Crypto (KxError(..), DHParams)
+import Network.TLS.Extension.Group (Group(..))
 import Network.TLS.Cipher
 import Network.TLS.Hooks
 import Network.TLS.Measurement
