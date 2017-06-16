c0cfdefc12930471fb7dbec1ee90e899d2cfb90a
Olivier Chéron <olivier.cheron@gmail.com>
2017-06-16 10:37:56 +0200

    Add test case related to server-certificate signature

--- core/Tests/Connection.hs
+++ core/Tests/Connection.hs
@@ -9,6 +9,7 @@ module Connection
     , arbitraryPairParams
     , arbitraryPairParamsWithVersionsAndCiphers
     , arbitraryClientCredential
+    , leafPublicKey
     , oneSessionManager
     , setPairParamsSessionManager
     , setPairParamsSessionResuming
@@ -92,6 +93,10 @@ arbitraryCredentialsOfEachType = do
          ) [ (PubKeyRSA pubKey, PrivKeyRSA privKey)
            , (PubKeyDSA dsaPub, PrivKeyDSA dsaPriv)
            ]
+
+leafPublicKey :: CertificateChain -> Maybe PubKey
+leafPublicKey (CertificateChain [])       = Nothing
+leafPublicKey (CertificateChain (leaf:_)) = Just (certPubKey $ signedObject $ getSigned leaf)
 
 arbitraryCipherPair :: Version -> Gen ([Cipher], [Cipher])
 arbitraryCipherPair connectVersion = do
