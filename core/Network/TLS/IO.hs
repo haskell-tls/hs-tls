40f4a80bc5427d52a273e9b8e75c743466ea04d8
Kazu Yamamoto <kazu@iij.ad.jp>
2017-12-08 14:19:23 +0900

    using <$>.

--- core/Network/TLS/IO.hs
+++ core/Network/TLS/IO.hs
@@ -103,7 +103,7 @@ recvPacket ctx = liftIO $ do
             pkt <- case pktRecv of
                     Right (Handshake hss) ->
                         ctxWithHooks ctx $ \hooks ->
-                            mapM (hookRecvHandshake hooks) hss >>= return . Right . Handshake
+                            Right . Handshake <$> mapM (hookRecvHandshake hooks) hss
                     _                     -> return pktRecv
             case pkt of
                 Right p -> withLog ctx $ \logging -> loggingPacketRecv logging $ show p
