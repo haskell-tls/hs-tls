8465ab467d70cbaa0ee3ae104a0c02a53c8ccff1
Kazu Yamamoto <kazu@iij.ad.jp>
2018-08-08 11:58:07 +0900

    adding sessionResumeOnlyOnce to SessionManager.

--- core/Network/TLS/Session.hs
+++ core/Network/TLS/Session.hs
@@ -15,16 +15,19 @@ import Network.TLS.Types
 -- | A session manager
 data SessionManager = SessionManager
     { -- | used on server side to decide whether to resume a client session.
-      sessionResume     :: SessionID -> IO (Maybe SessionData)
+      sessionResume         :: SessionID -> IO (Maybe SessionData)
+      -- | used on server side to decide whether to resume a client session for TLS 1.3 0RTT
+    , sessionResumeOnlyOnce :: SessionID -> IO (Maybe SessionData)
       -- | used when a session is established.
-    , sessionEstablish  :: SessionID -> SessionData -> IO ()
+    , sessionEstablish      :: SessionID -> SessionData -> IO ()
       -- | used when a session is invalidated.
-    , sessionInvalidate :: SessionID -> IO ()
+    , sessionInvalidate     :: SessionID -> IO ()
     }
 
 noSessionManager :: SessionManager
 noSessionManager = SessionManager
-    { sessionResume     = \_   -> return Nothing
-    , sessionEstablish  = \_ _ -> return ()
-    , sessionInvalidate = \_   -> return ()
+    { sessionResume         = \_   -> return Nothing
+    , sessionResumeOnlyOnce = \_   -> return Nothing
+    , sessionEstablish      = \_ _ -> return ()
+    , sessionInvalidate     = \_   -> return ()
     }
