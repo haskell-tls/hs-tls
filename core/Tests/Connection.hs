0043a0012020b1d34543e7925de6dcabae18f58c
Olivier Chéron <olivier.cheron@gmail.com>
2018-09-29 09:56:35 +0200

    Add isVersionEnabled

--- core/Tests/Connection.hs
+++ core/Tests/Connection.hs
@@ -12,6 +12,7 @@ module Connection
     , arbitraryPairParamsWithVersionsAndCiphers
     , arbitraryClientCredential
     , arbitraryRSACredentialWithUsage
+    , isVersionEnabled
     , isCustomDHParams
     , isLeafRSA
     , readClientSessionRef
@@ -162,6 +163,11 @@ arbitraryPairParamsAt connectVersion = do
         serAllowedVersions = [connectVersion]
     (clientCiphers, serverCiphers) <- arbitraryCipherPair connectVersion
     arbitraryPairParamsWithVersionsAndCiphers (allowedVersions, serAllowedVersions) (clientCiphers, serverCiphers)
+
+isVersionEnabled :: Version -> (ClientParams, ServerParams) -> Bool
+isVersionEnabled ver (cparams, sparams) =
+    (ver `elem` supportedVersions (serverSupported sparams)) &&
+    (ver `elem` supportedVersions (clientSupported cparams))
 
 arbitraryHashSignaturePair :: Gen ([HashAndSignatureAlgorithm], [HashAndSignatureAlgorithm])
 arbitraryHashSignaturePair = do
