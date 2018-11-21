0d514be8190e32ce74e06a053c80e4ee58d8260b
Olivier Chéron <olivier.cheron@gmail.com>
2018-11-21 19:12:30 +0100

    Fix typo in error message

--- core/Network/TLS/Handshake/Client.hs
+++ core/Network/TLS/Handshake/Client.hs
@@ -567,7 +567,7 @@ throwMiscErrorOnException msg e =
 onServerHello :: Context -> ClientParams -> [ExtensionID] -> Handshake -> IO (RecvState IO)
 onServerHello ctx cparams sentExts (ServerHello rver serverRan serverSession cipher compression exts) = do
     when (isDowngraded (supportedVersions $ clientSupported cparams) serverRan) $
-        throwCore $ Error_Protocol ("verion downgrade detected", True, IllegalParameter)
+        throwCore $ Error_Protocol ("version downgrade detected", True, IllegalParameter)
     when (rver == SSL2) $ throwCore $ Error_Protocol ("ssl2 is not supported", True, ProtocolVersion)
     -- find the compression and cipher methods that the server want to use.
     cipherAlg <- case find ((==) cipher . cipherID) (supportedCiphers $ ctxSupported ctx) of
