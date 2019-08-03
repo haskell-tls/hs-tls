e1b3f60ff31dfcf5a5e7457b671b965ed9d897d8
Olivier Chéron <olivier.cheron@gmail.com>
2019-08-03 09:18:15 +0200

    Remove unused lengthBytes

--- core/Network/TLS/Util/Serialization.hs
+++ core/Network/TLS/Util/Serialization.hs
@@ -2,11 +2,6 @@ module Network.TLS.Util.Serialization
     ( os2ip
     , i2osp
     , i2ospOf_
-    , lengthBytes
     ) where
 
-import Crypto.Number.Basic      (numBytes)
 import Crypto.Number.Serialize (os2ip, i2osp, i2ospOf_)
-
-lengthBytes :: Integer -> Int
-lengthBytes = numBytes
