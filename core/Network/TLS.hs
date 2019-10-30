b23064cf5a006d61da349de06e7e2c5f50a44b46
Kazu Yamamoto <kazu@iij.ad.jp>
2019-10-30 10:23:36 +0900

    sorting imports.

--- core/Network/TLS.hs
+++ core/Network/TLS.hs
@@ -139,28 +139,29 @@ module Network.TLS
     ) where
 
 import Network.TLS.Backend (Backend(..), HasBackend(..))
+import Network.TLS.Cipher
+import Network.TLS.Compression (CompressionC(..), Compression(..), nullCompression)
+import Network.TLS.Context
+import Network.TLS.Core
+import Network.TLS.Credentials
+import Network.TLS.Crypto (KxError(..), DHParams, DHPublic, Group(..))
+import Network.TLS.Handshake.State (HandshakeMode13(..))
+import Network.TLS.Hooks
+import Network.TLS.Measurement
+import Network.TLS.Parameters
+import Network.TLS.Session
 import Network.TLS.Struct ( TLSError(..), TLSException(..)
                           , HashAndSignatureAlgorithm, HashAlgorithm(..), SignatureAlgorithm(..)
                           , Header(..), ProtocolType(..), CertificateType(..)
                           , AlertDescription(..)
                           , ClientRandom(..), ServerRandom(..)
                           , Handshake)
-import Network.TLS.Crypto (KxError(..), DHParams, DHPublic, Group(..))
-import Network.TLS.Cipher
-import Network.TLS.Hooks
-import Network.TLS.Measurement
-import Network.TLS.Credentials
-import Network.TLS.Compression (CompressionC(..), Compression(..), nullCompression)
-import Network.TLS.Context
-import Network.TLS.Parameters
-import Network.TLS.Core
-import Network.TLS.Session
-import Network.TLS.X509
 import Network.TLS.Types
-import Network.TLS.Handshake.State (HandshakeMode13(..))
+import Network.TLS.X509
+
+import Data.ByteString as B
 import Data.X509 (PubKey(..), PrivKey(..))
 import Data.X509.Validation hiding (HostName)
-import Data.ByteString as B
 
 {-# DEPRECATED Bytes "Use Data.ByteString.Bytestring instead of Bytes." #-}
 type Bytes = B.ByteString
