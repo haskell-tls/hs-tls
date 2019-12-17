f0c3dbba74e180e129e9129e0e2fd365025d6caa
Kazu Yamamoto <kazu@iij.ad.jp>
2019-12-17 14:50:56 +0900

    explicitly documented default values.

--- core/Network/TLS/Session.hs
+++ core/Network/TLS/Session.hs
@@ -24,6 +24,7 @@ data SessionManager = SessionManager
     , sessionInvalidate     :: SessionID -> IO ()
     }
 
+-- | The session manager to do nothing.
 noSessionManager :: SessionManager
 noSessionManager = SessionManager
     { sessionResume         = \_   -> return Nothing
