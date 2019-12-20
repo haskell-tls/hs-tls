c5296ee5d48f870c86eb92b496c04f2ceb2b472d
Kazu Yamamoto <kazu@iij.ad.jp>
2019-12-20 12:41:20 +0900

    doc for getClientCertificateChain

--- core/Network/TLS.hs
+++ core/Network/TLS.hs
@@ -189,5 +189,10 @@ import Data.X509.Validation hiding (HostName)
 {-# DEPRECATED Bytes "Use Data.ByteString.Bytestring instead of Bytes." #-}
 type Bytes = B.ByteString
 
+-- | Getting certificates from a client, if any.
+--   Note that the certificates are not sent by a client
+--   on resumption even if client authentication is required.
+--   So, this API would be replaced by the one which can treat
+--   both cases of full-negotiation and resumption.
 getClientCertificateChain :: Context -> IO (Maybe CertificateChain)
 getClientCertificateChain ctx = usingState_ ctx S.getClientCertificateChain
