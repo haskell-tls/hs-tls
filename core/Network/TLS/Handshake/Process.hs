1c40e043080211f8c4bd42c8d6fd773d15df17e3
Kazu Yamamoto <kazu@iij.ad.jp>
2023-12-20 14:46:37 +0900

    expectClientCertificate calls processClientKeyXchg

--- core/Network/TLS/Handshake/Process.hs
+++ core/Network/TLS/Handshake/Process.hs
@@ -8,89 +8,33 @@ module Network.TLS.Handshake.Process (
     startHandshake,
 ) where
 
+import Control.Concurrent.MVar
+
 import Network.TLS.Context.Internal
-import Network.TLS.Crypto
-import Network.TLS.Handshake.Key
 import Network.TLS.Handshake.Random
 import Network.TLS.Handshake.State
 import Network.TLS.Handshake.State13
 import Network.TLS.Imports
-import Network.TLS.Packet
 import Network.TLS.Sending
 import Network.TLS.State
 import Network.TLS.Struct
 import Network.TLS.Struct13
-import Network.TLS.Types (MasterSecret (..), Role (..))
-
-import Control.Concurrent.MVar
-import Control.Monad.State.Strict (gets)
+import Network.TLS.Types (Role (..))
 
 processHandshake :: Context -> Handshake -> IO ()
 processHandshake ctx hs = do
     role <- usingState_ ctx getRole
     when (isHRR hs) $ usingHState ctx wrapAsMessageHash13
-    void $ updateHandshake ctx False hs
     case hs of
-        ClientKeyXchg content ->
-            when (role == ServerRole) $
-                processClientKeyXchg ctx content
-        _ -> return ()
+        ClientKeyXchg _
+            | role == ServerRole -> return ()
+        _ -> void $ updateHandshake ctx False hs
   where
     isHRR (ServerHello TLS12 srand _ _ _ _) = isHelloRetryRequest srand
     isHRR _ = False
 
 processHandshake13 :: Context -> Handshake13 -> IO ()
 processHandshake13 ctx = void . updateHandshake13 ctx
-
--- process the client key exchange message. the protocol expects the initial
--- client version received in ClientHello, not the negotiated version.
--- in case the version mismatch, generate a random master secret
-processClientKeyXchg :: Context -> ClientKeyXchgAlgorithmData -> IO ()
-processClientKeyXchg ctx (CKX_RSA encryptedPremaster) = do
-    (rver, role, random) <- usingState_ ctx $ do
-        (,,) <$> getVersion <*> getRole <*> genRandom 48
-    ePremaster <- decryptRSA ctx encryptedPremaster
-    masterSecret <- usingHState ctx $ do
-        expectedVer <- gets hstClientVersion
-        case ePremaster of
-            Left _ -> setMasterSecretFromPre rver role random
-            Right premaster -> case decodePreMasterSecret premaster of
-                Left _ -> setMasterSecretFromPre rver role random
-                Right (ver, _)
-                    | ver /= expectedVer -> setMasterSecretFromPre rver role random
-                    | otherwise -> setMasterSecretFromPre rver role premaster
-    logKey ctx (MasterSecret masterSecret)
-processClientKeyXchg ctx (CKX_DH clientDHValue) = do
-    rver <- usingState_ ctx getVersion
-    role <- usingState_ ctx getRole
-
-    serverParams <- usingHState ctx getServerDHParams
-    let params = serverDHParamsToParams serverParams
-    unless (dhValid params $ dhUnwrapPublic clientDHValue) $
-        throwCore $
-            Error_Protocol "invalid client public key" IllegalParameter
-
-    dhpriv <- usingHState ctx getDHPrivate
-    let premaster = dhGetShared params dhpriv clientDHValue
-    masterSecret <- usingHState ctx $ setMasterSecretFromPre rver role premaster
-    logKey ctx (MasterSecret masterSecret)
-processClientKeyXchg ctx (CKX_ECDH bytes) = do
-    ServerECDHParams grp _ <- usingHState ctx getServerECDHParams
-    case decodeGroupPublic grp bytes of
-        Left _ ->
-            throwCore $
-                Error_Protocol "client public key cannot be decoded" IllegalParameter
-        Right clipub -> do
-            srvpri <- usingHState ctx getGroupPrivate
-            case groupGetShared clipub srvpri of
-                Just premaster -> do
-                    rver <- usingState_ ctx getVersion
-                    role <- usingState_ ctx getRole
-                    masterSecret <- usingHState ctx $ setMasterSecretFromPre rver role premaster
-                    logKey ctx (MasterSecret masterSecret)
-                Nothing ->
-                    throwCore $
-                        Error_Protocol "cannot generate a shared secret on ECDH" IllegalParameter
 
 -- initialize a new Handshake context (initial handshake or renegotiations)
 startHandshake :: Context -> Version -> ClientRandom -> IO ()
--- core/Network/TLS/Handshake/Server/TLS12.hs
+++ core/Network/TLS/Handshake/Server/TLS12.hs
@@ -4,9 +4,11 @@ module Network.TLS.Handshake.Server.TLS12 (
     recvClientSecondFlight12,
 ) where
 
+import Control.Monad.State.Strict (gets)
 import qualified Data.ByteString as B
 
 import Network.TLS.Context.Internal
+import Network.TLS.Crypto
 import Network.TLS.Handshake.Common
 import Network.TLS.Handshake.Key
 import Network.TLS.Handshake.Process
@@ -15,7 +17,9 @@ import Network.TLS.Handshake.Signature
 import Network.TLS.Handshake.State
 import Network.TLS.IO
 import Network.TLS.Imports
+import Network.TLS.Packet hiding (getSession)
 import Network.TLS.Parameters
+import Network.TLS.Sending
 import Network.TLS.Session
 import Network.TLS.State
 import Network.TLS.Struct
@@ -79,7 +83,11 @@ recvClientCCC sparams ctx = runRecvState ctx (RecvStateHandshake expectClientCer
 
     -- cannot use RecvStateHandshake, as the next message could be a ChangeCipher,
     -- so we must process any packet, and in case of handshake call processHandshake manually.
-    expectClientKeyExchange (ClientKeyXchg _) = return $ RecvStatePacket expectCertificateVerify
+    expectClientKeyExchange hs@(ClientKeyXchg ckx) = do
+        -- processClientKeyXchg requres updateHandshake, sigh
+        void $ updateHandshake ctx False hs
+        processClientKeyXchg ctx ckx
+        return $ RecvStatePacket expectCertificateVerify
     expectClientKeyExchange p = unexpected (show p) (Just "client key exchange")
 
     -- Check whether the client correctly signed the handshake.
@@ -142,3 +150,53 @@ expectChangeCipher :: Context -> Packet -> IO (RecvState IO)
 expectChangeCipher ctx ChangeCipherSpec = do
     return $ RecvStateHandshake $ expectFinished ctx
 expectChangeCipher _ p = unexpected (show p) (Just "change cipher")
+
+-- process the client key exchange message. the protocol expects the initial
+-- client version received in ClientHello, not the negotiated version.
+-- in case the version mismatch, generate a random master secret
+processClientKeyXchg :: Context -> ClientKeyXchgAlgorithmData -> IO ()
+processClientKeyXchg ctx (CKX_RSA encryptedPremaster) = do
+    (rver, role, random) <- usingState_ ctx $ do
+        (,,) <$> getVersion <*> getRole <*> genRandom 48
+    ePremaster <- decryptRSA ctx encryptedPremaster
+    masterSecret <- usingHState ctx $ do
+        expectedVer <- gets hstClientVersion
+        case ePremaster of
+            Left _ -> setMasterSecretFromPre rver role random
+            Right premaster -> case decodePreMasterSecret premaster of
+                Left _ -> setMasterSecretFromPre rver role random
+                Right (ver, _)
+                    | ver /= expectedVer -> setMasterSecretFromPre rver role random
+                    | otherwise -> setMasterSecretFromPre rver role premaster
+    logKey ctx (MasterSecret masterSecret)
+processClientKeyXchg ctx (CKX_DH clientDHValue) = do
+    rver <- usingState_ ctx getVersion
+    role <- usingState_ ctx getRole
+
+    serverParams <- usingHState ctx getServerDHParams
+    let params = serverDHParamsToParams serverParams
+    unless (dhValid params $ dhUnwrapPublic clientDHValue) $
+        throwCore $
+            Error_Protocol "invalid client public key" IllegalParameter
+
+    dhpriv <- usingHState ctx getDHPrivate
+    let premaster = dhGetShared params dhpriv clientDHValue
+    masterSecret <- usingHState ctx $ setMasterSecretFromPre rver role premaster
+    logKey ctx (MasterSecret masterSecret)
+processClientKeyXchg ctx (CKX_ECDH bytes) = do
+    ServerECDHParams grp _ <- usingHState ctx getServerECDHParams
+    case decodeGroupPublic grp bytes of
+        Left _ ->
+            throwCore $
+                Error_Protocol "client public key cannot be decoded" IllegalParameter
+        Right clipub -> do
+            srvpri <- usingHState ctx getGroupPrivate
+            case groupGetShared clipub srvpri of
+                Just premaster -> do
+                    rver <- usingState_ ctx getVersion
+                    role <- usingState_ ctx getRole
+                    masterSecret <- usingHState ctx $ setMasterSecretFromPre rver role premaster
+                    logKey ctx (MasterSecret masterSecret)
+                Nothing ->
+                    throwCore $
+                        Error_Protocol "cannot generate a shared secret on ECDH" IllegalParameter
