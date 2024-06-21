da0cf8ec0e536fd37824676a6101881b0688df05
Kazu Yamamoto <kazu@iij.ad.jp>
2024-06-21 10:43:03 +0900

    terminating clientDNS

--- tls/util/Client.hs
+++ tls/util/Client.hs
@@ -8,6 +8,7 @@ module Client (
     clientDNS,
 ) where
 
+import qualified Data.ByteString.Base16 as BS16
 import qualified Data.ByteString.Lazy.Char8 as CL8
 import Network.Socket
 import Network.TLS
@@ -37,11 +38,14 @@ clientHTTP11 aux@Aux{..} paths ctx = do
             <> "\r\n"
     consume ctx aux
 
-clientDNS aux paths ctx = do
+clientDNS :: Cli
+clientDNS Aux{..} _paths ctx = do
     sendData
         ctx
         "\x00\x2c\xdc\xe3\x01\x00\x00\x01\x00\x00\x00\x00\x00\x01\x03\x77\x77\x77\x07\x65\x78\x61\x6d\x70\x6c\x65\x03\x63\x6f\x6d\x00\x00\x01\x00\x01\x00\x00\x29\x04\xd0\x00\x00\x00\x00\x00\x00"
-    consume ctx aux
+    bs <- recvData ctx
+    auxShow $ "Reply: " <> BS16.encode bs
+    auxShow "\n"
 
 consume :: Context -> Aux -> IO ()
 consume ctx Aux{..} = loop
