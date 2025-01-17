ee9e770d59803667c3ff2be02a4d26cd97e1071b
Kazu Yamamoto <kazu@iij.ad.jp>
2025-01-17 12:11:44 +0900

    proper ordering

--- tls/Network/TLS/Packet13.hs
+++ tls/Network/TLS/Packet13.hs
@@ -24,6 +24,8 @@ import Network.TLS.Struct13
 import Network.TLS.Types
 import Network.TLS.Wire
 
+----------------------------------------------------------------
+
 encodeHandshake13 :: Handshake13 -> ByteString
 encodeHandshake13 hdsk = pkt
   where
@@ -45,15 +47,6 @@ encodeHandshake13' (ServerHello13 random session cipherId exts) = runPut $ do
     putWord16 $ fromCipherId cipherId
     putWord8 0 -- compressionID nullCompression
     putExtensions exts
-encodeHandshake13' (EncryptedExtensions13 exts) = runPut $ putExtensions exts
-encodeHandshake13' (CertRequest13 reqctx exts) = runPut $ do
-    putOpaque8 reqctx
-    putExtensions exts
-encodeHandshake13' (Certificate13 reqctx (TLSCertificateChain cc) ess) = encodeCertificate13 reqctx cc ess
-encodeHandshake13' (CertVerify13 (DigitallySigned hs sig)) = runPut $ do
-    putSignatureHashAlgorithm hs
-    putOpaque16 sig
-encodeHandshake13' (Finished13 (VerifyData dat)) = runPut $ putBytes dat
 encodeHandshake13' (NewSessionTicket13 life ageadd nonce label exts) = runPut $ do
     putWord32 life
     putWord32 ageadd
@@ -61,6 +54,15 @@ encodeHandshake13' (NewSessionTicket13 life ageadd nonce label exts) = runPut $ 
     putOpaque16 label
     putExtensions exts
 encodeHandshake13' EndOfEarlyData13 = ""
+encodeHandshake13' (EncryptedExtensions13 exts) = runPut $ putExtensions exts
+encodeHandshake13' (Certificate13 reqctx (TLSCertificateChain cc) ess) = encodeCertificate13 reqctx cc ess
+encodeHandshake13' (CertRequest13 reqctx exts) = runPut $ do
+    putOpaque8 reqctx
+    putExtensions exts
+encodeHandshake13' (CertVerify13 (DigitallySigned hs sig)) = runPut $ do
+    putSignatureHashAlgorithm hs
+    putOpaque16 sig
+encodeHandshake13' (Finished13 (VerifyData dat)) = runPut $ putBytes dat
 encodeHandshake13' (KeyUpdate13 UpdateNotRequested) = runPut $ putWord8 0
 encodeHandshake13' (KeyUpdate13 UpdateRequested) = runPut $ putWord8 1
 
@@ -68,6 +70,19 @@ encodeHandshakeHeader13 :: HandshakeType -> Int -> ByteString
 encodeHandshakeHeader13 ty len = runPut $ do
     putWord8 (fromHandshakeType ty)
     putWord24 len
+
+encodeCertificate13
+    :: CertReqContext -> CertificateChain -> [[ExtensionRaw]] -> ByteString
+encodeCertificate13 reqctx cc ess = runPut $ do
+    putOpaque8 reqctx
+    putOpaque24 (runPut $ mapM_ putCert $ zip certs ess)
+  where
+    CertificateChainRaw certs = encodeCertificateChain cc
+    putCert (certRaw, exts) = do
+        putOpaque24 certRaw
+        putExtensions exts
+
+----------------------------------------------------------------
 
 decodeHandshakes13 :: MonadError TLSError m => ByteString -> m [Handshake13]
 decodeHandshakes13 bs = case decodeHandshakeRecord13 bs of
@@ -90,13 +105,13 @@ decodeHandshake13
     :: HandshakeType -> ByteString -> Either TLSError Handshake13
 decodeHandshake13 ty = runGetErr ("handshake[" ++ show ty ++ "]") $ case ty of
     HandshakeType_ServerHello -> decodeServerHello13
-    HandshakeType_Finished -> decodeFinished13
-    HandshakeType_EncryptedExtensions -> decodeEncryptedExtensions13
-    HandshakeType_CertRequest -> decodeCertRequest13
-    HandshakeType_Certificate -> decodeCertificate13
-    HandshakeType_CertVerify -> decodeCertVerify13
     HandshakeType_NewSessionTicket -> decodeNewSessionTicket13
     HandshakeType_EndOfEarlyData -> return EndOfEarlyData13
+    HandshakeType_EncryptedExtensions -> decodeEncryptedExtensions13
+    HandshakeType_Certificate -> decodeCertificate13
+    HandshakeType_CertRequest -> decodeCertRequest13
+    HandshakeType_CertVerify -> decodeCertVerify13
+    HandshakeType_Finished -> decodeFinished13
     HandshakeType_KeyUpdate -> decodeKeyUpdate13
     (HandshakeType x) -> fail $ "Unsupported HandshakeType " ++ show x
 
@@ -110,21 +125,21 @@ decodeServerHello13 = do
     exts <- fromIntegral <$> getWord16 >>= getExtensions
     return $ ServerHello13 random session cipherid exts
 
-decodeFinished13 :: Get Handshake13
-decodeFinished13 = Finished13 . VerifyData <$> (remaining >>= getBytes)
+decodeNewSessionTicket13 :: Get Handshake13
+decodeNewSessionTicket13 = do
+    life <- getWord32
+    ageadd <- getWord32
+    nonce <- getOpaque8
+    label <- getOpaque16
+    len <- fromIntegral <$> getWord16
+    exts <- getExtensions len
+    return $ NewSessionTicket13 life ageadd nonce label exts
 
 decodeEncryptedExtensions13 :: Get Handshake13
 decodeEncryptedExtensions13 =
     EncryptedExtensions13 <$> do
         len <- fromIntegral <$> getWord16
         getExtensions len
-
-decodeCertRequest13 :: Get Handshake13
-decodeCertRequest13 = do
-    reqctx <- getOpaque8
-    len <- fromIntegral <$> getWord16
-    exts <- getExtensions len
-    return $ CertRequest13 reqctx exts
 
 decodeCertificate13 :: Get Handshake13
 decodeCertificate13 = do
@@ -142,19 +157,19 @@ decodeCertificate13 = do
         exts <- getExtensions len
         return (3 + l + 2 + len, (cert, exts))
 
+decodeCertRequest13 :: Get Handshake13
+decodeCertRequest13 = do
+    reqctx <- getOpaque8
+    len <- fromIntegral <$> getWord16
+    exts <- getExtensions len
+    return $ CertRequest13 reqctx exts
+
 decodeCertVerify13 :: Get Handshake13
 decodeCertVerify13 =
     CertVerify13 <$> (DigitallySigned <$> getSignatureHashAlgorithm <*> getOpaque16)
 
-decodeNewSessionTicket13 :: Get Handshake13
-decodeNewSessionTicket13 = do
-    life <- getWord32
-    ageadd <- getWord32
-    nonce <- getOpaque8
-    label <- getOpaque16
-    len <- fromIntegral <$> getWord16
-    exts <- getExtensions len
-    return $ NewSessionTicket13 life ageadd nonce label exts
+decodeFinished13 :: Get Handshake13
+decodeFinished13 = Finished13 . VerifyData <$> (remaining >>= getBytes)
 
 decodeKeyUpdate13 :: Get Handshake13
 decodeKeyUpdate13 = do
@@ -163,14 +178,3 @@ decodeKeyUpdate13 = do
         0 -> return $ KeyUpdate13 UpdateNotRequested
         1 -> return $ KeyUpdate13 UpdateRequested
         x -> fail $ "Unknown request_update: " ++ show x
-
-encodeCertificate13
-    :: CertReqContext -> CertificateChain -> [[ExtensionRaw]] -> ByteString
-encodeCertificate13 reqctx cc ess = runPut $ do
-    putOpaque8 reqctx
-    putOpaque24 (runPut $ mapM_ putCert $ zip certs ess)
-  where
-    CertificateChainRaw certs = encodeCertificateChain cc
-    putCert (certRaw, exts) = do
-        putOpaque24 certRaw
-        putExtensions exts
