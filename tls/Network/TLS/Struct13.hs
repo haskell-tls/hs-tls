11b0c3cdab64f9dc09f73d3135a3643144e0077b
Kazu Yamamoto <kazu@iij.ad.jp>
2025-01-17 11:20:39 +0900

    proper ordering

--- tls/Network/TLS/Struct13.hs
+++ tls/Network/TLS/Struct13.hs
@@ -31,8 +31,8 @@ data Handshake13
     | NewSessionTicket13 Second Word32 TicketNonce SessionIDorTicket [ExtensionRaw]
     | EndOfEarlyData13
     | EncryptedExtensions13 [ExtensionRaw]
-    | CertRequest13 CertReqContext [ExtensionRaw]
     | Certificate13 CertReqContext TLSCertificateChain [[ExtensionRaw]]
+    | CertRequest13 CertReqContext [ExtensionRaw]
     | CertVerify13 DigitallySigned
     | Finished13 VerifyData
     | KeyUpdate13 KeyUpdate
@@ -44,11 +44,11 @@ type CertReqContext = ByteString
 {- FOURMOLU_DISABLE -}
 typeOfHandshake13 :: Handshake13 -> HandshakeType
 typeOfHandshake13 ServerHello13{}         = HandshakeType_ServerHello
-typeOfHandshake13 EndOfEarlyData13{}      = HandshakeType_EndOfEarlyData
 typeOfHandshake13 NewSessionTicket13{}    = HandshakeType_NewSessionTicket
+typeOfHandshake13 EndOfEarlyData13{}      = HandshakeType_EndOfEarlyData
 typeOfHandshake13 EncryptedExtensions13{} = HandshakeType_EncryptedExtensions
-typeOfHandshake13 CertRequest13{}         = HandshakeType_CertRequest
 typeOfHandshake13 Certificate13{}         = HandshakeType_Certificate
+typeOfHandshake13 CertRequest13{}         = HandshakeType_CertRequest
 typeOfHandshake13 CertVerify13{}          = HandshakeType_CertVerify
 typeOfHandshake13 Finished13{}            = HandshakeType_Finished
 typeOfHandshake13 KeyUpdate13{}           = HandshakeType_KeyUpdate
