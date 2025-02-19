8be4a0b9ba1a52fb629838156c9cac53782121fb
Kazu Yamamoto <kazu@iij.ad.jp>
2025-02-19 09:42:34 +0900

    removing processHandshake13

--- tls/Network/TLS/Handshake/Process.hs
+++ tls/Network/TLS/Handshake/Process.hs
@@ -2,7 +2,6 @@
 -- process handshake message received
 module Network.TLS.Handshake.Process (
     processHandshake12,
-    processHandshake13,
 ) where
 
 import Network.TLS.Context.Internal
@@ -10,7 +9,6 @@ import Network.TLS.Handshake.State13
 import Network.TLS.IO.Encode
 import Network.TLS.Imports
 import Network.TLS.Struct
-import Network.TLS.Struct13
 
 processHandshake12 :: Context -> Handshake -> IO ()
 processHandshake12 ctx hs = do
@@ -19,6 +17,3 @@ processHandshake12 ctx hs = do
   where
     isHRR (ServerHello TLS12 srand _ _ _ _) = isHelloRetryRequest srand
     isHRR _ = False
-
-processHandshake13 :: Context -> Handshake13 -> IO ()
-processHandshake13 ctx = void . updateHandshake13 ctx
