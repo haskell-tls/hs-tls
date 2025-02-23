8f42edf72771012ca201909f897c7776d4f05a89
Kazu Yamamoto <kazu@iij.ad.jp>
2025-02-23 09:00:35 +0900

    show for secrets

--- tls/Network/TLS/Types/Secret.hs
+++ tls/Network/TLS/Types/Secret.hs
@@ -13,16 +13,29 @@ data ApplicationSecret
 
 data ResumptionSecret
 
-newtype BaseSecret a = BaseSecret ByteString deriving (Show)
-newtype AnyTrafficSecret a = AnyTrafficSecret ByteString deriving (Show)
+newtype BaseSecret a = BaseSecret ByteString
+
+instance Show (BaseSecret a) where
+    show (BaseSecret bs) = showBytesHex bs
+
+newtype AnyTrafficSecret a = AnyTrafficSecret ByteString
+
+instance Show (AnyTrafficSecret a) where
+    show (AnyTrafficSecret bs) = showBytesHex bs
 
 -- | A client traffic secret, typed with a parameter indicating a step in the
 -- TLS key schedule.
-newtype ClientTrafficSecret a = ClientTrafficSecret ByteString deriving (Show)
+newtype ClientTrafficSecret a = ClientTrafficSecret ByteString
+
+instance Show (ClientTrafficSecret a) where
+    show (ClientTrafficSecret bs) = showBytesHex bs
 
 -- | A server traffic secret, typed with a parameter indicating a step in the
 -- TLS key schedule.
-newtype ServerTrafficSecret a = ServerTrafficSecret ByteString deriving (Show)
+newtype ServerTrafficSecret a = ServerTrafficSecret ByteString
+
+instance Show (ServerTrafficSecret a) where
+    show (ServerTrafficSecret bs) = showBytesHex bs
 
 data SecretTriple a = SecretTriple
     { triBase :: BaseSecret a
@@ -40,4 +53,7 @@ data SecretPair a = SecretPair
 type TrafficSecrets a = (ClientTrafficSecret a, ServerTrafficSecret a)
 
 -- Main secret for TLS 1.2 or earlier.
-newtype MainSecret = MainSecret ByteString deriving (Show)
+newtype MainSecret = MainSecret ByteString
+
+instance Show MainSecret where
+    show (MainSecret bs) = showBytesHex bs
