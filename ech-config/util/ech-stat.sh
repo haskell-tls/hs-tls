d2771680286a8d4157d6d8ed1808e6db6d30cc60
Kazu Yamamoto <kazu@iij.ad.jp>
2025-09-17 07:41:31 +0900

    using dug -f short

--- ech-config/util/ech-stat.sh
+++ ech-config/util/ech-stat.sh
@@ -1,12 +1,12 @@
 doms=$(cat dom-list.txt)
 for dom in $doms; do
   printf '%s' $dom
-  if dug $dom a 1> /dev/null 2>&1; then
+  n=$(dug -f short $dom a | wc -l | awk '{print $1}')
+  if [ $n -ne 0 ]; then
       printf ' a'
-      if dug $dom https | grep 'ech=' 1> /dev/null 2>&1; then
+      if dug -f short $dom https | grep 'ech=' 1> /dev/null 2>&1; then
         printf ' ech'
       fi
   fi
   printf '\n'
 done
-
