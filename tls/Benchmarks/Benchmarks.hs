376be0454bfa1f570c8501080660eb4e1be2a2d5
KovalevDima <onarainydayillwinanyway@gmail.com>
2025-10-05 17:22:04 +0300

    Fix benchmarks

--- tls/Benchmarks/Benchmarks.hs
+++ tls/Benchmarks/Benchmarks.hs
@@ -156,17 +156,11 @@ main =
         [ bgroup
             "connection"
             -- not sure the number actually make sense for anything. improve ..
-            [ benchConnection (getParams SSL3 blockCipher) small "SSL3-256 bytes"
-            , benchConnection (getParams TLS10 blockCipher) small "TLS10-256 bytes"
-            , benchConnection (getParams TLS11 blockCipher) small "TLS11-256 bytes"
-            , benchConnection (getParams TLS12 blockCipher) small "TLS12-256 bytes"
+            [ benchConnection (getParams TLS12 blockCipher) small "TLS12-256 bytes"
             ]
         , bgroup
             "resumption"
-            [ benchResumption (getParams SSL3 blockCipher) small "SSL3-256 bytes"
-            , benchResumption (getParams TLS10 blockCipher) small "TLS10-256 bytes"
-            , benchResumption (getParams TLS11 blockCipher) small "TLS11-256 bytes"
-            , benchResumption (getParams TLS12 blockCipher) small "TLS12-256 bytes"
+            [ benchResumption (getParams TLS12 blockCipher) small "TLS12-256 bytes"
             ]
         , -- Here we try to measure TLS12 and TLS13 performance with AEAD ciphers.
           -- Resumption and a larger message can be a demonstration of the symmetric
@@ -178,8 +172,6 @@ main =
             [ cipher_DHE_RSA_WITH_AES_128_GCM_SHA256
             , cipher_DHE_RSA_WITH_AES_256_GCM_SHA384
             , cipher_DHE_RSA_WITH_CHACHA20_POLY1305_SHA256
-            , cipher_ECDHE_ECDSA_WITH_AES_128_CCM
-            , cipher_ECDHE_ECDSA_WITH_AES_128_CCM_8
             , cipher_ECDHE_RSA_WITH_AES_128_GCM_SHA256
             , cipher_ECDHE_RSA_WITH_AES_256_GCM_SHA384
             , cipher_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
